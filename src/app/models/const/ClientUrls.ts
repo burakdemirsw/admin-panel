d00b3b1c09259f266aec10a11d492ef73975d555
burak demir <demir.burock96@gmail.com>
2024-05-14 12:36:22 +0300

    Polar

--- src/app/models/const/ClientUrls.ts
+++ src/app/models/const/ClientUrls.ts
@@ -1,10 +1,25 @@
 export enum ClientUrls {
-  // baseUrl = "http://localhost:7178/api", //bu pc
-  // baseUrl = "http://212.156.46.206:7179/api", //36
-  //baseUrl = "https://www.davyebkmapi.com/api", //36
   baseUrl = "http://78.135.64.6:3074/api",
   baseUrl2 = "http://212.156.46.206:4201/search-qr/", //bu pc asdsa
 }
 
 
-//bu alan sadece polarstarda gözükmeli
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
--- src/app/models/const/dist/ClientUrls.js
+++ src/app/models/const/dist/ClientUrls.js
@@ -3,10 +3,25 @@ exports.__esModule = true;
 exports.ClientUrls = void 0;
 var ClientUrls;
 (function (ClientUrls) {
-    // baseUrl = "http://localhost:7178/api", //bu pc
-    // baseUrl = "http://212.156.46.206:7179/api", //36
-    //baseUrl = "https://www.davyebkmapi.com/api", //36
     ClientUrls["baseUrl"] = "http://78.135.64.6:3074/api";
     ClientUrls["baseUrl2"] = "http://212.156.46.206:4201/search-qr/";
 })(ClientUrls = exports.ClientUrls || (exports.ClientUrls = {}));
-//bu alan sadece polarstarda gözükmeli
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
+//POLAR
