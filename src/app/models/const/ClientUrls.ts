15280a6d4af266c25929a5cf925fdaa4770df4e1
burak demir <demir.burock96@gmail.com>
2024-05-14 12:19:02 +0300

    polar-deneme

--- src/app/models/const/ClientUrls.ts
+++ src/app/models/const/ClientUrls.ts
@@ -7,3 +7,4 @@ export enum ClientUrls {
 }
 
 
+//bu alan sadece polarstarda gözükmeli
--- src/app/models/const/dist/ClientUrls.js
+++ src/app/models/const/dist/ClientUrls.js
@@ -9,3 +9,4 @@ var ClientUrls;
     ClientUrls["baseUrl"] = "http://78.135.64.6:3074/api";
     ClientUrls["baseUrl2"] = "http://212.156.46.206:4201/search-qr/";
 })(ClientUrls = exports.ClientUrls || (exports.ClientUrls = {}));
+//bu alan sadece polarstarda gözükmeli
