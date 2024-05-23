08ad2a4973be1f5f48eb59ca76a9d955a57a045c
burak demir <demir.burock96@gmail.com>
2024-05-23 23:44:25 +0300

    23.05

---
+++ src/app/components/Order/marketplace-invoices/marketplace-invoices.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MarketplaceInvoicesComponent } from './marketplace-invoices.component';
+
+describe('MarketplaceInvoicesComponent', () => {
+  let component: MarketplaceInvoicesComponent;
+  let fixture: ComponentFixture<MarketplaceInvoicesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MarketplaceInvoicesComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MarketplaceInvoicesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/Order/marketplace-invoices/marketplace-invoices.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplace-invoices',
+  standalone: true,
+  imports: [],
+  templateUrl: './marketplace-invoices.component.html',
+  styleUrl: './marketplace-invoices.component.css'
+})
+export class MarketplaceInvoicesComponent {
+
+}
