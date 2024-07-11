3dbfa1791e79059d0a237cc3d3ec25f7bba13406
burak demir <demir.burock96@gmail.com>
2024-07-11 14:44:32 +0300

    SÜMEYRA

--- src/app/models/const/ClientUrls.ts
+++ src/app/models/const/ClientUrls.ts
@@ -1,6 +1,6 @@
 export const ClientUrls = {
-  baseUrl: "http://localhost:7178/api", // Local PC MISIGO
-  //baseUrl: "http://192.168.2.38:7178/api",
+  baseUrl: "http://localhost:7178/api", // Local PC SÜMEYRA
+  //baseUrl: "http://192.168.2.36:7178/api",
   //baseUrl: "http://78.135.64.6:3074/api", // Uncomment as needed
   baseUrl2: "http://212.156.46.206:4201/search-qr/", // Local PC for another API
   cargoFirms: [
