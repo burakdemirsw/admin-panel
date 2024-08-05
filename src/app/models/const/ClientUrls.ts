d6a262a7796a85a4fb7555e0ab81dda617ae2162
burak demir <demir.burock96@gmail.com>
2024-08-06 01:05:43 +0300

    06.08

--- src/app/models/const/ClientUrls.ts
+++ src/app/models/const/ClientUrls.ts
@@ -1,7 +1,7 @@
 
 export enum ClientUrls {
-  baseUrl = "http://localhost:7178/api", //local
-  //baseUrl = "https://www.davyebkmapi.com/api", // yeni server
+  // baseUrl = "http://localhost:7178/api", //local
+  baseUrl = "https://www.davyebkmapi.com/api", // yeni server
   // baseUrl = "http://212.156.46.206:7179/api", //ilk server
   baseUrl2 = "http://212.156.46.206:4201/search-qr/",
   photoUrl = "http://212.156.46.206:7676/photo/web1/"
