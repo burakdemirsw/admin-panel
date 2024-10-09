a5d541ec301a36412b8bdb2e297644a2b292aede
burak demir <demir.burock96@gmail.com>
2024-10-09 14:15:13 +0300

    09.10

--- src/app/models/const/ClientUrls.ts
+++ src/app/models/const/ClientUrls.ts
@@ -1,6 +1,6 @@
 export const ClientUrls = {
-  //baseUrl: "http://localhost:7178/api", // Local PC
-  baseUrl: "http://78.135.64.6:3074/api", // Uncomment as needed
+  baseUrl: "http://localhost:7178/api", // Local PC
+  //baseUrl: "http://78.135.64.6:3074/api", // Uncomment as needed
   baseUrl2: "http://212.156.46.206:4201/search-qr/", // Local PC for another APIasd  asdas
   cargoFirms: [
     { name: "MNG", id: 1 },
