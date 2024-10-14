4f3f4b1e80197e722f82a07e34645668cf8a165d
burak demir <demir.burock96@gmail.com>
2024-10-14 15:55:03 +0300

    14.10

---
+++ src/app/bank-process-list/bank-process-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-process-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './bank-process-list.component.html',
+  styleUrl: './bank-process-list.component.css'
+})
+export class BankProcessListComponent {
+
+}
